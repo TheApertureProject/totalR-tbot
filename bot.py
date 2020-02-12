043e49e6be0517ba2f6529b5ba1ad5c770844dee
poulpette <apertureproject.me@gmail.com>
2020-02-12 23:46:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def get_why(): # Get a random question from nekos.life
 
 def get_vine(): # Get a random example of def from ud.com
     r = requests.get(url="http://api.urbandictionary.com/v0/random")
-    if r.status_code >= 200 and r.status < 400:
+    if r.status_code >= 200 and r.status_code < 400:
         down = 0
         r2 = json.loads(r.text)
         r3 = r2["list"][0]["example"]
