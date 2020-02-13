20041024e0e4ad8cfda7ec34fcca49530d20f290
poulpette <apertureproject.me@gmail.com>
2020-02-13 08:10:54 +0100

    Except urbandict errors

--- bot.py
+++ bot.py
@@ -29,14 +29,14 @@ def get_vine(): # Get a random example of def from ud.com
         r = requests.get(url="http://api.urbandictionary.com/v0/random")
         r2 = json.loads(r.text)
         r3 = r2["list"][0]["example"]
-            if len(r3) > 281:
-                while len(r3) > 281:
-                    r = requests.get(url="http://api.urbandictionary.com/v0/random")
-                    r2 = json.loads(r.text)
-                    r3 = r2["list"][0]["example"]
-                r4 = r3.replace('[', '')
-                r5 = r4.replace(']', '')
-                return r5
+        if len(r3) > 281:
+            while len(r3) > 281:
+                r = requests.get(url="http://api.urbandictionary.com/v0/random")
+                r2 = json.loads(r.text)
+                r3 = r2["list"][0]["example"]
+            r4 = r3.replace('[', '')
+            r5 = r4.replace(']', '')
+            return r5
     except Exception as e:
         print(e)
         return 1
